bf36db796c00247f5f0bc1fac66aae47c4a52b04
Sahar Galimidi <sahargalimidi@gmail.com>
2024-03-13 17:33:14 +0200

    fix route to be with single POST endpoint

--- graph_route_finder/app/routes.py
+++ graph_route_finder/app/routes.py
@@ -5,8 +5,7 @@ import os
 
 main = Blueprint('main', __name__)
 
-
-@main.route('/', methods=['GET', 'POST'])
+@main.route('/', methods=['POST'])
 def index():
     file_path = os.path.join(os.path.dirname(__file__), 'graph_example.json')
     with open(file_path) as file:
@@ -16,27 +15,24 @@ def index():
     path_distance = None
     closest_vertex = None
 
-    if request.method == 'POST':
-        action = request.form.get('action')
+    action = request.form.get('action')
 
-        if action == 'find_shortest_path':
-            start = request.form.get('start')
-            end = request.form.get('end')
-            shortest_path, path_distance = graph.find_shortest_path(start, end)
-            if shortest_path:
-                graph.show_graph(shortest_path)
+    if action == 'find_shortest_path':
+        start = request.form.get('start')
+        end = request.form.get('end')
+        shortest_path, path_distance = graph.find_shortest_path(start, end)
+        if shortest_path:
+            graph.show_graph(shortest_path)
 
-        elif action == 'find_closest_vertex':
-            vertex = request.form.get('closest')
-            vertex = tuple(map(float, vertex.replace(
-                '(', '').replace(')', '').split(',')))
-            closest_vertex = graph.find_closest_vertex(vertex)
-            if closest_vertex:
-                if isinstance(closest_vertex, str):
-                    graph.show_graph(shortest_vertex=closest_vertex)
-                else:
-                    graph.show_graph(
-                        closest_vertex_info=closest_vertex, source_vertex=vertex)
+    elif action == 'find_closest_vertex':
+        vertex = request.form.get('closest')
+        vertex = tuple(map(float, vertex.replace('(', '').replace(')', '').split(',')))
+        closest_vertex = graph.find_closest_vertex(vertex)
+        if closest_vertex:
+            if isinstance(closest_vertex, str):
+                graph.show_graph(shortest_vertex=closest_vertex)
+            else:
+                graph.show_graph(closest_vertex_info=closest_vertex, source_vertex=vertex)
 
     return render_template('index.html', vertices=graph.adjacency_dict.keys(),
                            shortest_path=shortest_path, path_distance=path_distance, closest_vertex=closest_vertex)
